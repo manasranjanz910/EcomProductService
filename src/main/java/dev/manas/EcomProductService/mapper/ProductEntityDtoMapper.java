bd8bf3e5f02da15dfc711c37b54ccb6f86df6dfb
manasranjan <ranjanmanas.z910@gmail.com>
2024-06-09 21:14:50 +0530

    application.properties modified for RDS

--- src/main/java/dev/manas/EcomProductService/mapper/ProductEntityDtoMapper.java
+++ src/main/java/dev/manas/EcomProductService/mapper/ProductEntityDtoMapper.java
@@ -2,7 +2,10 @@ package dev.manas.EcomProductService.mapper;
 
 import dev.manas.EcomProductService.dto.ProductRequestDto;
 import dev.manas.EcomProductService.dto.ProductResponseDto;
+import dev.manas.EcomProductService.entity.Cart;
 import dev.manas.EcomProductService.entity.Product;
+
+import java.util.UUID;
 
 public class ProductEntityDtoMapper {
 
@@ -17,11 +20,13 @@ public class ProductEntityDtoMapper {
         productResponseDto.setPrice(product.getPrice());
         productResponseDto.setRating(product.getRating());
         productResponseDto.setProductId(product.getId());
+
         return productResponseDto;
     }
     public static Product convertProductRequestDtoToProduct(ProductRequestDto productRequestDto)
     {
         Product product = new Product();
+
         product.setTitle(productRequestDto.getTitle());
         product.setRating(0);
         product.setDescription(product.getDescription());
